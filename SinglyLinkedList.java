1e94b6fd5e1d2bf335b2dc124a30894bcdab4f77
Edwinwong9991 <wongvoonguo@gmail.com>
2026-09-01 17:07:11 +0800

    done

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -67,14 +67,83 @@ public class SinglyLinkedList<E> {
 
     // Write your codes below
     public String toString(){
-     
+        StringBuilder sb = new StringBuilder();
+        Node current = head;
+        if (current == null){
+            return "";
+        }
+
+        while (current != tail){
+            sb.append(current.getElement());
+            current = current.getNext();
+        }
+        sb.append(current.getElement());
+
+        return sb.toString();
+
     }
 
     public E removeLast(){
-      
+        Node prev = head;
+        Node curr = head;
+        if (head == null){
+            return null;
+        }
+        if (head.getNext() == null){
+            head = null;
+            tail = null;
+            return curr.getElement();
+        } else {
+            curr = head.getNext();
+        }
+
+        while (curr != tail){
+            prev = prev.getNext();
+            curr = curr.getNext();
+        }
+
+        prev.setNext(null);
+        tail = prev;
+        return curr.getElement();
     }
 
-    public void reverse(){       
+    public void reverse(){  
+        if (head == null){
+            return;
+        }
+        Node prev = head;
+        Node curr = null;
+        Node move = null;
+
+        if (head.getNext() != null && head.getNext().getNext() != null){
+            curr = head.getNext();
+            move = head.getNext().getNext();
+            prev.setNext(null); 
+            tail = prev;
+            while (move != null){
+                curr.setNext(prev);
+                prev = curr;
+                curr = move;
+                move = move.getNext();
+            }
+            curr.setNext(prev);
+            prev = curr;
+            head = prev;
+
+
+        } else if (head.getNext() != null){
+            curr = head.getNext();
+            curr.setNext(prev);
+            prev.setNext(null);
+            head = curr;
+            tail = prev;
+            return;
+        }
+
+
+
+
+            
                  
     }
 }
